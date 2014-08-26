ad163593aa3d50f6b7b5fd9ba97deb43a00c8d16
jacqueline <jacqueline@Macintosh-3.local>
2014-08-26 15:05:39 -0500

    added median to stats.py

--- stats.py
+++ stats.py
@@ -1,31 +1,22 @@
+import math
+
 def mean(vals):
-    """Computes the mean from a list of values."""
-    try:
-        total = float(sum(vals))
-        length = len(vals)
-    except TypeError:
-        raise TypeError("The list was not numbers.")
-    except:
-        print "Something unknown happened with the list."
-    return float(total)/length
-
-def mode(vals):
-    """Computes the mode from a list of values."""
-    pass
-
-def std(vals):
-    """Computes the standard deviation from a list of values."""
-    n = len(vals)
-    if n == 0:
-        return 0.0
-    mu = sum(vals) / n
-    if mu == 1e500:
-        return NotImplemented
-    var = 0.0
-    for val in vals:
-        var = var + (val - mu)**2
-    return (var / n)**0.5
-
-def var(vals):
-    """Computes the variance from a list of values."""
-    pass
+	"""Computes the mean from a list of values"""
+	total = sum(vals)
+	length = len(vals)
+	return float(total)/float(length)
+	
+def test_float_mean():
+	assert(mean[1,2]== 1.5)
+	
+def test_mean():
+	assert(mean([2,4])== 3)
+	
+def median(vals):
+    vals.sort()
+    length = len(vals)
+    index = length / 2
+    if length % 2 == 0:
+       return mean([vals[index], vals[index - 1]])
+    else:
+       return vals[index]
